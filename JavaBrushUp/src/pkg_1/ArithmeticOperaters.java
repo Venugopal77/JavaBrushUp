25c713b44e307313f28cc4f9d258511e2703558c
Venugopal77 <venucse2013@gmail.com>
2022-09-06 11:58:09 +0530

    Java Sample script

---
+++ JavaBrushUp/src/pkg_1/ArithmeticOperaters.java
@@ -0,0 +1,19 @@
+package pkg_1;
+
+public class ArithmeticOperaters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=500;
+		int num2=5;
+		
+		//num1++;
+		int num3= num1++;
+		//num3=1+num1;
+		
+		System.out.println(num3);
+		System.out.println(num1);
+	}
+
+}
---
+++ JavaBrushUp/src/pkg_1/PrimitiveDatatypes.java
@@ -0,0 +1,22 @@
+package pkg_1;
+
+public class PrimitiveDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte b1 = 127;
+		short sh1 = 484;
+		int in1 = 2564649;
+		long lo1 = 21648941;
+		float fl1 = 456.4234536f;
+		double d1 = 34234.324234;
+		char ch1 = 'a';
+		boolean b2 = false;
+		String Str = "Engineer";
+				
+		System.out.println(Str);
+		
+	}
+
+}
